85ac90f9c8eee0d6b97bccca17d291e49e7cc06a
alexandr-morozMD <alexandrmoroz.md@gmail.com>
2025-10-07 01:44:54 +0300

    UPDATE +vercel.py and json in back/app

--- backend/app/vercel.py
+++ backend/app/vercel.py
@@ -1,4 +1,9 @@
-from app.main import app  # Vercel ищет переменную app
+# backend/app/vercel.py
+from app.main import app
 
-# Vercel handler
-handler = app
+# ⚠️ НЕ нужно создавать handler = app
+# Vercel сам подхватит переменную app
+# просто убедись, что это объект FastAPI, не функция
+
+# пример проверки:
+# print(type(app))  # <class 'fastapi.applications.FastAPI'>

0eb725e98207b79b88311997c01c1077a4aa0784
alexandr-morozMD <alexandrmoroz.md@gmail.com>
2025-10-08 15:29:32 +0300

    try: fix vercel 2

--- backend/app/vercel.py
+++ backend/app/vercel.py
@@ -1,1 +1,10 @@
-from backend.app.main import app
+import os
+import sys
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+BACKEND_ROOT = os.path.dirname(CURRENT_DIR)
+
+if BACKEND_ROOT not in sys.path:
+    sys.path.insert(0, BACKEND_ROOT)
+
+from app.main import app  # noqa: E402
